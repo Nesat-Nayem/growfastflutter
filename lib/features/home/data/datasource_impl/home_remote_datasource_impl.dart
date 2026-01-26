357a19f9820a293fa85d5492598017c446dc97f5
Nesat Nayem <nesatnayem042@gmail.com>
2026-01-26 11:56:03 +0600

    with local url

--- lib/features/home/data/datasource_impl/home_remote_datasource_impl.dart
+++ lib/features/home/data/datasource_impl/home_remote_datasource_impl.dart
@@ -12,11 +12,11 @@ class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
 
   HomeRemoteDataSourceImpl(this.dio);
 
-  static const String url = "https://growfirst.org/api/home-page";
+  static const String url = "http://growsercode.test/api/home-page";
   static const String recentUrl =
-      "https://growfirst.org/api/customer/recent-searches?location=dewas";
+      "http://growsercode.test/api/customer/recent-searches?location=dewas";
   static const String servicesByTypeUrl =
-      "https://growfirst.org/api/customer/recent-searches";
+      "http://growsercode.test/api/customer/recent-searches";
 
   @override
   Future<HomePageResponse> getHomePageData() async {
